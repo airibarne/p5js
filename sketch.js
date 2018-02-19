e8be7f59ff01ef0108f263048283846ea93df81b
Albert Iribarne <albert.iribarne@gmail.com>
2018-02-19 19:17:06 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,35 @@
-function setup() {
-	width = windowWidth;
-	height = 300;
-	createCanvas(width, height);
+// CASE 0: no node specified
+// Canvas is auto-generated and appended to body.
+var sketch = function (p) {
+
+  p.setup = function () {
+    width = p.windowWidth;
+	height = 250;
+	p.createCanvas(width, height);
 	numberOfWaves = 12;
-	noStroke();
 	theta = 0;
-}
+	speed = 0.04;
+	p.noStroke();
+  };
 
-function draw() {
-	background(0);
+  p.draw = function () {
+    p.background(0);
 	for(i=0; i<numberOfWaves; i++) {
-		drawWave(i);
+		p.drawWave(i);
 	}
-	theta += 0.04;
-}
+	theta += speed
+  };
 
-
-function drawWave(i) {
-	randomSeed(123);
+  p.drawWave = function (i) {
+    p.randomSeed(123);
 	for(x=0; x<=width; x+=10) {
-		amplitude = map(i, 0, numberOfWaves-1, 1, 100);
-		offSet = 2*PI/width*x
-		y = height/2 + sin(theta*offSet)*amplitude;
-		fill(random(255)/numberOfWaves*i,random(255)/numberOfWaves*i,random(255)/numberOfWaves*i);
-		ellipse(x, y, 4, 4);
-	}
-};
-/*function drawWave(i) {
-	randomSeed(123);
-	for(x=0; x<=width; x+=10) {
-	  	amplitude = map(i,0,numberOfWaves-1, 1, 100);
-	  	offSet = 2*PI/width*x;
-	  	y = height/2 + sin(theta+offSet)*amplitude;
-	  	fill(random(255)/numberOfWaves*i,random(255)/numberOfWaves*i,random(255)/numberOfWaves*i);
-	  	ellipse(x, y, 4, 4);
+	  	amplitude = p.map(i,0,numberOfWaves-1, 1, 100);
+	  	offSet = 2*p.PI/width*x;
+	  	y = height/2 + p.sin(theta*offSet)*amplitude;
+	  	p.fill(p.random(255)/numberOfWaves*i,p.random(255)/numberOfWaves*i,p.random(255)/numberOfWaves*i);
+	  	p.ellipse(x, y, 4, 4);
   	}
-};*/
+  };
+};
+
+new p5(sketch);
